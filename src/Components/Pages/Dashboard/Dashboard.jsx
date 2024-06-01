6aad391445eb0fd749e969d57f824131aad23844
Robiul Islam <robiulislam.me2022@gmail.com>
2024-06-02 03:56:27 +0600

    worker dashboard

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -3,25 +3,47 @@ import { NavLink, Outlet } from "react-router-dom";
 import Workers from "./Workers/Workers";
 import TaskCreator from "./TaskCreator/TaskCreator";
 import Admin from "./Admin/Admin";
+import { BiSolidDashboard } from "react-icons/bi";
+import { ImProfile } from "react-icons/im";
+import { LiaPagerSolid } from "react-icons/lia";
 
 
 const Dashboard = () => {
 
-    const isWorker = false;
+    const isWorker = true;
     const isTaskCreator = false;
-    const isAdmin = true;
+    const isAdmin = false;
 
 
     return (
         <div className="flex">
-            <div className="w-64 sticky top-1 min-h-screen bg-blue-950 text-white">
-                <ul className="menu p-4">
+            <div className="w-72 sticky top-1 min-h-screen bg-green-600 text-white">
+                <ul className="menu p-6">
+                    <h2 className="text-3xl font-bold">$ InboxDoller</h2>
+                    <div className="divider"></div>
 
+                    <h2 className="flex text-xl font-bold gap-1 mb-4 text-black"><ImProfile className="mt-1 text-xl" /> PROFILE</h2>
+                    <div className=" bg-white p-4 card">
+                        <div className="avatar">
+                            <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                            <div className="text-black ml-5">
+                                <h2>Name :</h2>
+                                <h2>Role :</h2>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="divider"></div>
+                    <h2 className="flex text-xl font-bold gap-1 mb-4 text-black"><BiSolidDashboard className="mt-1" /> DASHBOARD</h2>
                     {
                         isWorker ? <> <Workers /> </> : '' || isTaskCreator ? <> <TaskCreator /> </> : '' || isAdmin ? <> <Admin /> </> : ''
                     }
                     {/* shared nav links */}
                     <div className="divider"></div>
+                    <h2 className="flex text-xl text-black font-bold gap-1 mb-4 border-b"><LiaPagerSolid className="mt-1" /> PAGES</h2>
                     <li>
                         <NavLink to="/">
                             <FaHome></FaHome>
@@ -41,7 +63,7 @@ const Dashboard = () => {
             </div>
 
             {/* dashboard content */}
-            <div className="flex-1 p-8">
+            <div className="flex-1">
                 <Outlet />
             </div>
         </div>
--- src/Components/Pages/Dashboard/Workers/WHome/WHome.jsx
+++ src/Components/Pages/Dashboard/Workers/WHome/WHome.jsx
@@ -1,10 +1,55 @@
+import { IoNotifications } from "react-icons/io5";
 
 
 const WHome = () => {
     return (
-        <div>
-            <h2>WHome.............</h2>
-        </div>
+        <section>
+            <div className="h-[85px] bg-green-600 px-4">
+                <div className="flex justify-end gap-4 p-6">
+                    <div>
+                        <span className="border-r-2 border-black px-4">$ Total Coin</span>
+                    </div>
+                    <div className="indicator">
+                        <span className="indicator-item ">99+</span>
+                        <button ><IoNotifications className="text-3xl" /></button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="p-4">
+                <div className="stats w-full  shadow">
+                    <div className="stat">
+                        <div className="stat-title">Total Earning</div>
+                        <div className="stat-value text-primary">25.6K</div>
+                        <div className="stat-desc">21% more than last month</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-title">Task Complete</div>
+                        <div className="stat-value text-secondary">2.6M</div>
+                        <div className="stat-desc">15% more than last month</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-value">86%</div>
+                        <div className="stat-title">Tasks done</div>
+                        <div className="stat-desc text-secondary">31 tasks remaining</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="flex p-4">
+                <div className="bg-red-800 w-full">
+                    SIDEEEEEEEEEEEE111111111111111111111111111111111111111111
+                </div>
+
+                <div className="bg-yellow-600">
+                    SIDEEEEEEEEEEEE2
+                </div>
+            </div>
+        </section>
     );
 };
 
